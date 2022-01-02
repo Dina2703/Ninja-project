1dca7b1089cd3a64bbf02154a7454b0399ccbde3
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-02 13:20:39 -0500

    inline style in react-component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,17 @@ function Navbar() {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        {/*outter curly braces represent a dynamic value, inner is JS object(styling object) with key-value pair */}
+        <a
+          href="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
       </div>
     </nav>
   );
