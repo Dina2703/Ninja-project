5fbc80b5ea83bed9e60331deab34abb8c097a860
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-02 15:18:52 -0500

    Outputting Lists

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,25 @@
 import { useState } from "react";
 
-//useState hook gives as a way to make a reactive value and also provides us with a way to change that value whenever we want.
 function Home() {
-  {
-    /* this value is not reactive value, so if its change not going to change the UI, because React not watching ins value, as it does with reactive values 
-  let name = "sam"; */
-  }
-  //useState() value could be anything, number, string, array, object, boolean
-  const [name, setName] = useState("Sam");
-  const [age, setAge] = useState(20);
-
-
-  const handleClick = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
     {
-      /*this value is reactive value so if it changes at any point it's going to change in the template(UI) as well. So when we use this function setName to change the value, that triggers react to re-render the component, when it re-renders we have that new value of name beacuse it's been updated */
-    }
-    setName("Dina");
-    setAge(21);
-  };
-
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Home Page</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      {/*handleClick with () gets fired immediately, before the Click occurs, we don't neet to add() */}
-      <button onClick={handleClick}>Click Me</button>
+      {/*the key attr React needs to keep track of every item in an array in the DOM */}
+      {blogs.map((blog) => (
+        <div key={blog.id} className="blog-preview">
+          <h2> {blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
