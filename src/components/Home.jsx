96e4b5939742a1e6afae1b7bb69b2e9a118ed3c2
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-02 18:48:24 -0500

    useEffect hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 function Home() {
@@ -17,6 +17,12 @@ function Home() {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+  //useEffect rans(fires) for every render. it's very useful for things  like fetching data. 
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(blogs);
+  });
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
