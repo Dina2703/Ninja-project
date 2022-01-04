bf5996e844fdd840bd63367fa7903b14d2b591fe
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-03 23:08:12 -0500

    handling Fetch Errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 function Home() {
   const [blogs, setBlogs] = useState();
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   //useEffect rans(fires) for every render(first time when component  first loads(first initial render). then every time the state changes we re-render the template(UI). it's very useful for things  like fetching data.
   //An empty dependency array ([]) -- it only runs it once, when the component renders. that we can pass as a second argument into useEffect hook, to make sure that this hook only runs the function after the first initial render, then when the state changes it won't run the function again,
@@ -12,14 +13,21 @@ function Home() {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then((data) => {
           console.log(data);
           setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsPending(false);
         });
-
-      setIsPending(false);
     }, 1000);
   }, []);
 
@@ -42,6 +50,7 @@ function Home() {
   return (
     <div className="home">
       {/*the condition below: if blogs null or any falsy value, it will never riches the right hand side of the logical and evaluates it, which is the BlogList component, when blogs gets the value of data(now it's truthy) the logical will evaluate the right side, it outputs it to the screen. The thing on the right is going to be output, if the thing on the left is true*/}
+      {error && <div>{error}</div>}
       {isPending && <div>Loading... </div>}
       {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
     </div>
