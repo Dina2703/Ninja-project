9caa01d1b7c2ba87b9cd7f2dd515ee2b7e83a67d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-03 22:35:44 -0500

    Conditional Loading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,19 +3,24 @@ import BlogList from "./BlogList";
 
 function Home() {
   const [blogs, setBlogs] = useState();
+  const [isPending, setIsPending] = useState(true);
 
   //useEffect rans(fires) for every render(first time when component  first loads(first initial render). then every time the state changes we re-render the template(UI). it's very useful for things  like fetching data.
   //An empty dependency array ([]) -- it only runs it once, when the component renders. that we can pass as a second argument into useEffect hook, to make sure that this hook only runs the function after the first initial render, then when the state changes it won't run the function again,
   //now [name] is a dependency -- it only runs it once and when the target(name) chages its value, means the useEffect watches the change of the value of name in the state(onClick button 'change name'), and it triggers the useEffect function to run. so the second argument to useEffect is to control when this function runs
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+        });
+
+      setIsPending(false);
+    }, 1000);
   }, []);
 
   //with async and await
@@ -37,6 +42,7 @@ function Home() {
   return (
     <div className="home">
       {/*the condition below: if blogs null or any falsy value, it will never riches the right hand side of the logical and evaluates it, which is the BlogList component, when blogs gets the value of data(now it's truthy) the logical will evaluate the right side, it outputs it to the screen. The thing on the right is going to be output, if the thing on the left is true*/}
+      {isPending && <div>Loading... </div>}
       {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
     </div>
   );
