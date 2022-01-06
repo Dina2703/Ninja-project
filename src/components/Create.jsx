d977f174d375faf13fe9fe24b3071ec61e33490e
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-05 19:32:54 -0500

    Controlled Inputs (forms)

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
 
 function Create() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
   return (
-    <div>
+    <div className="create">
       <h2>Add a new Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="title"
+          type="text"
+          required
+        />
+        <label>Blog body:</label>
+        <textarea
+          placeholder="start typing"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">Mario</option>
+          <option value="sam">Sam</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Create
+export default Create;
