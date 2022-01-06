987905df4d287f37c8c20828a19b2d476dee1d75
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-05 20:05:21 -0500

    Making a POST Request

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -4,6 +4,7 @@ function Create() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handelSubmit = (e) => {
     //to prevent the page refreshing and auto clean the form.
@@ -11,6 +12,22 @@ function Create() {
     //creating a blog object with title. body and author proporties, but id(json-server does it automatically), that will be saved in db.json file
     const blog = { title, body, author };
     console.log(blog);
+    setIsPending(true);
+
+    //since the fetch request is asynchronous and it returns a promise
+    //I set setTimeout to give a time to Adding blog button to show-up for a while
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs/", {
+        method: "POST",
+        //below tells that with this request we're sending a json data
+        headers: { "Content-type": "application/json" },
+        //it turns the blog object into a json string
+        body: JSON.stringify(blog),
+      }).then(() => {
+        console.log("new blog added");
+        setIsPending(false);
+      });
+    }, 1000);
   };
 
   return (
@@ -41,10 +58,8 @@ function Create() {
           <option value="mario">Mario</option>
           <option value="sam">Sam</option>
         </select>
-        <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
   );
